77876e141305790f3ce8fb175c78d841bd4bebdb
”yk2001525“ <1272689242@qq.com>
2021-08-23 00:44:00 +0800

    08月23日00:43:57

---
+++ src/components/FileSearch.js
@@ -0,0 +1,85 @@
+import React, { useState,useEffect,useRef } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faSearch,faTimes} from '@fortawesome/free-solid-svg-icons'
+import PropTypes from 'prop-types'
+
+FileSearch.propTypes = {
+    title: PropTypes.string,
+    onFileSearch: PropTypes.func.isRequired
+}
+
+FileSearch.defaultProps={
+    title:'我的云文档'
+}
+
+export default function FileSearch({ title, onFileSearch }) {
+  const [inputActive, setInputActive] = useState(false);
+  const [value, setValue] = useState("");
+
+  let node = useRef(null)
+
+  const closeSearch = (e)=>{
+    e.preventDefault()
+    setInputActive(false)
+    setValue('')
+  }
+
+  useEffect(()=>{
+      const handleInputEvent = (event)=>{
+          const {keyCode} = event
+          if(keyCode === 13 && inputActive){
+              onFileSearch(value)
+          }else if(keyCode === 27 && inputActive){
+            closeSearch(event)
+          }
+      }
+      document.addEventListener('keyup',handleInputEvent)
+      return ()=>{
+          document.removeEventListener('keyup',handleInputEvent)
+      }
+  })
+  useEffect(()=>{
+      if(inputActive){
+        node.current.focus()
+      }
+  },[inputActive])
+  return (
+    <div className="alert alert-primary d-flex justify-content-between align-items-center">
+      {!inputActive && 
+        <>
+          <span>{title}</span>
+          <button
+            type="button"
+            className="icon-button"
+            onClick={() => {
+              setInputActive(true);
+            }}
+          >
+            <FontAwesomeIcon title="搜索" size="lg" icon={faSearch}/>
+          </button>
+          </>
+
+      }
+      {inputActive && 
+        <>
+          <input
+
+            className="form-control"
+            onChange={(e) => {
+              setValue(e.target.value);
+            }}
+            ref={node}
+            value={value}
+          />
+          <button
+            type="button"
+            className="icon-button"
+            onClick={closeSearch}
+          >
+            <FontAwesomeIcon title="关闭" size="lg" icon={faTimes}/>
+          </button>
+        </>
+      }
+    </div>
+  );
+}
